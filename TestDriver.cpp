bcbbfd109f24a39a3758d83c2999fd75be2aafb0
whitel2 <whitel2@acad.sunybroome.edu>
2025-04-05 12:00:27 -0400

    Add files via upload
    Added methods to upload the dictionary to a vector and to get words from the vector.

--- TestDriver.cpp
+++ TestDriver.cpp
@@ -1,31 +1,56 @@
 /*********************************************************************************
- * Test Driver for Task 2
- * 
- * Tests the RadixSort method to sort words lexicographically
+ * Test Driver for Task 1
+ *
+ * Tests the DictionaryReader class to read in the dictionary
  *
  * Author - Lauren White
- * Date - 4/3/25
+ * Date - 4/5/25
  ********************************************************************************/
 
 #include <iostream>
-//#include <iomanip>
 #include <string>
-#include <vector>
-
-#include "RadixSort.h"
+#include "DictionaryReader.h"
 
 using namespace std;
 
-int main()
+int main() 
 {
-    vector<int> arr{ 170, 245, 315, 174, 326, 32, 17, 26 };
-    RadixSort(arr);
+    string filename;
 
-    cout << "Sorted: " << endl;
-    for (int num : arr)
+    //Get the dictionary file name to read in
+    cout << "Enter the dictionary file name: ";
+    getline(cin, filename);
+
+    DictionaryReader dictionary(filename);
+    vector<string> words = dictionary.getDictionary();
+
+    //If there are no words in the dictionary vector, output an error message and exit the function
+    if (words.empty()) 
     {
-        cout << num << " ";
+        cout << "Error: Dictionary file is empty." << endl;
+        return 1;
     }
+
+    //Output the words from the dictionary
+    cout << "Words from the dictionary file:" << endl;
+    for (const string& word : words) 
+    {
+        cout << word << endl;
+    }
+
+    /*
+    //Visual Studios would not output the entire list, so I had CoPilot write a test to make sure the first 50 words were correctly processed
+    cout << "First 50 words from " << filename << ":" << std::endl;
+
+    size_t count = 0; // Counter for the number of words printed
+    for (const string& word : words) {
+        cout << word << endl;
+        count++;
+
+        if (count >= 50) { // Check if we've printed 50 words
+            break; // Exit the loop
+        }
+    }*/
 
     return 0;
 }
